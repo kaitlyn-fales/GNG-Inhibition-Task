73e528b6ecf3a55f971e74517f7e8d1afba7ce24
kaitlyn-fales <krf5429@psu.edu>
2026-03-11 15:26:10 -0400

    group analysis SPM scripts

---
+++ SPM/group_analysis_SPM.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=5
+#SBATCH --ntasks=1 
+#SBATCH --mem-per-cpu=4gb
+#SBATCH --time=24:00:00
+#SBATCH --account=statsresearch_sc_default
+#SBATCH --output=output_%A_%a.out
+#SBATCH --array=1-4
+
+# Get started
+echo " "
+echo "Job started on `hostname` at `date`"
+echo " "
+
+# Environment setup
+module purge
+module use /gpfs/group/RISE/sw7/modules
+module load r/4.2.1
+
+export R_LIBS="~/R_packages"
+
+Rscript group_analysis_SPM.R 
+
+# Finish up
+echo " "
+echo "Job Ended at `date`"
+echo " "
+
+
+
+
+
+
